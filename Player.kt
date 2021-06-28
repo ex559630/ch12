95ad681d240df34ef9612a59ee82a771ff705c53
shinre <ex559630@gmail.com>
2021-06-28 19:19:04 +0800

    c12-8

--- Player.kt
+++ Player.kt
@@ -1,6 +1,13 @@
 class Player {
     val name = "佐藤"
+    get() =field.capitalize()
+
     fun castFireball(numFireballs:Int=2){
         println("A glass of FireBall springs into existence.(x$numFireballs)")
     }
 }
+/*
+field 不允許直接定義
+get 讀取屬性
+set 設定屬性
+ */
