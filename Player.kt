f6bf3a54b7cd2985ca87fcc038821b620a305e0c
shinre <ex559630@gmail.com>
2021-06-28 19:14:21 +0800

    c12-5

--- Player.kt
+++ Player.kt
@@ -1,5 +1,5 @@
 class Player {
-
+    val name = "佐藤"
     fun castFireball(numFireballs:Int=2){
         println("A glass of FireBall springs into existence.(x$numFireballs)")
     }
--- game.kt
+++ game.kt
@@ -8,15 +8,7 @@ fun main(){
     player.castFireball()
 
     var auraVisible = healthPoint >=50 && isBlessed || isImmortal
-    val karma = (Math.pow(Math.random(),(110-healthPoint)/100.0)*20).toInt()
-    var auraColor = when(karma){
-        in 0..5 -> "Red"
-        in 6..10 -> "Orange"
-        in 11..15 -> "Purple"
-        in 16..20 -> "Green"
-        else -> "沒有光環"
-    }
-    println("$healthPoint $karma")
+
     val healthStatus= when(healthPoint){
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
@@ -28,6 +20,6 @@ fun main(){
         in 15..74 -> "look pretty hurt."
         else -> "is in awful condition"
     }
-    println("光環顏色: $auraColor , 是否受祝福: ${if(isBlessed) "是的" else "否"}   ")
-    println("$name $healthStatus")
+    //println("光環顏色: $auraColor , 是否受祝福: ${if(isBlessed) "是的" else "否"}   ")
+    //println("$name $healthStatus")
 }
